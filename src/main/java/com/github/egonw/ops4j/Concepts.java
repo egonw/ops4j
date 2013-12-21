87187212331b9a5fef053f989c9565e06d341fc6
Egon Willighagen <egon.willighagen@gmail.com>
2013-12-21 21:34:42 +0100

    Added the CW search methods

---
+++ src/main/java/com/github/egonw/ops4j/Concepts.java
@@ -0,0 +1,68 @@
+/* Copyright (C) 2013  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package com.github.egonw.ops4j;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.HttpException;
+import org.apache.http.client.ClientProtocolException;
+
+public class Concepts extends AbstractOPS4JClient {
+
+	private Concepts(String server, String appID, String appKey) throws MalformedURLException {
+		this.server = server;
+		if (!this.server.endsWith("/")) this.server += "/";
+		new URL(this.server); // validate the server URL
+		this.appID = appID;
+		this.appKey = appKey;
+	}
+
+	public static Concepts getInstance(String server, String apiID, String appKey) throws MalformedURLException {
+		return new Concepts(server, apiID, appKey);
+	}
+
+	public String freetext(String text, Object... objects) throws ClientProtocolException, IOException, HttpException {
+		Map<String,String> params = new HashMap<String,String>();
+		params.put("q", text);
+		return runRequest(server + "search/freetext", params);
+	}
+
+	public String freetextByTag(String text, String uuid, Object... objects)
+	throws ClientProtocolException, IOException, HttpException {
+		Map<String,String> params = new HashMap<String,String>();
+		params.put("q", text);
+		params.put("uuid", uuid);
+		return runRequest(server + "search/byTag", params);
+	}
+
+	public String description(String conceptWikiID, Object... objects)
+	throws ClientProtocolException, IOException, HttpException {
+		Map<String,String> params = new HashMap<String,String>();
+		params.put("uuid", conceptWikiID);
+		return runRequest(server + "getConceptDescription", params);
+	}
+}
---
+++ src/test/java/com/github/egonw/ops4j/ConceptsTest.java
@@ -0,0 +1,66 @@
+/* Copyright (C) 2013  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package com.github.egonw.ops4j;
+
+import java.io.IOException;
+
+import org.apache.http.HttpException;
+import org.apache.http.client.ClientProtocolException;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ConceptsTest extends AbstractOPS4JTest {
+
+	{ super.pickUpConfig(); }
+
+	@Test
+	public void freetext() throws ClientProtocolException, IOException, HttpException {
+		Concepts client = Concepts.getInstance(super.server, super.appID, super.appKey);
+		Assert.assertNotNull(client);
+		String turtle = client.freetext("water");
+		Assert.assertNotNull(turtle);
+		Assert.assertTrue(turtle.contains("prefix"));
+		Assert.assertTrue(turtle.contains("water"));
+	}
+
+	@Test
+	public void byTag() throws ClientProtocolException, IOException, HttpException {
+		Concepts client = Concepts.getInstance(super.server, super.appID, super.appKey);
+		Assert.assertNotNull(client);
+		String turtle = client.freetextByTag("water", "07a84994-e464-4bbf-812a-a4b96fa3d197");
+		Assert.assertNotNull(turtle);
+		Assert.assertTrue(turtle.contains("prefix"));
+		Assert.assertTrue(turtle.contains("water"));
+	}
+
+	@Test
+	public void description() throws ClientProtocolException, IOException, HttpException {
+		Concepts client = Concepts.getInstance(super.server, super.appID, super.appKey);
+		Assert.assertNotNull(client);
+		String turtle = client.description("8f7f8fe0-f7ad-4192-b57b-e00120d0beee");
+		Assert.assertNotNull(turtle);
+		Assert.assertTrue(turtle.contains("prefix"));
+		Assert.assertTrue(turtle.contains("Isoprene"));
+	}
+}
