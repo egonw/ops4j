7a0a8672bc9c3dc09078701d4f1734f9f4dd79eb
Egon Willighagen <egon.willighagen@gmail.com>
2015-11-11 17:50:37 +0100

    Support the 'all' approach

--- src/main/java/com/github/egonw/ops4j/Targets.java
+++ src/main/java/com/github/egonw/ops4j/Targets.java
@@ -64,6 +64,13 @@ public class Targets extends AbstractOPS4JClient {
 		return runRequest(server + "target/pharmacology/pages", params, objects);
 	}
 
+	public String pharmacologyAll(String uri, Object... objects) throws ClientProtocolException, IOException, HttpException {
+		Map<String,String> params = new HashMap<String,String>();
+		params.put("uri", uri);
+		params.put("_pageSize", "all");
+		return runRequest(server + "target/pharmacology/pages", params, objects);
+	}
+
 	public String classifications(String uri, Object... objects) throws ClientProtocolException, IOException, HttpException {
 		Map<String,String> params = new HashMap<String,String>();
 		params.put("uri", uri);
